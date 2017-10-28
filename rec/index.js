de1fef7b2b485afd77c24dcc3d845a2aa2716996
DeemoHarlos <cces4415@gmail.com>
2017-10-28 17:05:31 +0800

    new version add team description and fix color

--- rec/index.js
+++ rec/index.js
@@ -10,3 +10,26 @@ function loaded(e,f){
 function clicked(e,f){
 	e.addEventListener('click',f)
 }
+
+/*Page Flipping*/
+$('.page-button')[0].style.borderRadius = '25%'
+$('.page-button').forEach((e,i,a)=>{
+	clicked(e,()=>{
+		$('.page-button').forEach((e,i,a)=>{e.style.borderRadius = '50%'})
+		e.style.borderRadius = '25%'
+	})
+})
+ 
+curpage = -1
+$('#teams>.team').forEach((e,i,a)=>{
+	clicked(e,()=>{
+		if(curpage >= 0)
+			$('#teams .des')[curpage].style.display = 'none'
+		if(i != curpage){
+			$('#teams .des')[i].style.display = 'block'
+			curpage = i
+		}
+		else
+			curpage = -1
+	})
+})
