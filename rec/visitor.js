c69c0affbd938be22464da9cf23b28242deb61c3
DeemoHarlos <cces4415@gmail.com>
2017-11-01 02:38:23 +0800

    visitor counter fix

--- rec/visitor.js
+++ rec/visitor.js
@@ -2,7 +2,7 @@ var req = new XMLHttpRequest()
 var server = 'http://deemo.pw:11519'
 req.open('POST',server)
 req.setRequestHeader('Content-Type','application/X-www-form-urlencoded')
-req.send('website=deemo.life&userAgent=' + window.navigator.userAgent)
+req.send('website=infas.club\/2018\/rec&userAgent=' + window.navigator.userAgent)
 req.onreadystatechange = function () {
   if(req.readyState === XMLHttpRequest.DONE)
     console.log(server + ' responded with status ' + req.status)
