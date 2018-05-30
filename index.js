62d13ac9bd5c16eada2958e2373df13c6495459c
Allen Chou <s3131212@gmail.com>
2018-05-30 21:05:53 +0800

    fix typo

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ $(function(){
         e.preventDefault()
         var target = $(this.getAttribute('href'))
         if( target.length ) {
-            event.preventDefault()
+            e.preventDefault()
             $('html, body').stop().animate({
                 scrollTop: target.offset().top - 70
             }, 500)
